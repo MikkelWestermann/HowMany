691c7284b7b203ba375d7e7b70d1f3ea5a5b6980
Mikkel Westermann <mikkelwestermann@Mikkels-iMac.local>
2018-12-18 20:48:42 +0100

    Updates

---
+++ precache-manifest.d8bfebabf233d5b18a9ebe09bfc88180.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c502541393e606ab6af",
+    "url": "/HowMany/static/css/main.23fa24a3.chunk.css"
+  },
+  {
+    "revision": "8c502541393e606ab6af",
+    "url": "/HowMany/static/js/main.8c502541.chunk.js"
+  },
+  {
+    "revision": "217e004b83d12cfd2d72",
+    "url": "/HowMany/static/js/1.217e004b.chunk.js"
+  },
+  {
+    "revision": "c74f6c2f0dfe9bfeba45",
+    "url": "/HowMany/static/js/runtime~main.c74f6c2f.js"
+  },
+  {
+    "revision": "98dc3740497d51432ffff9783ad184d0",
+    "url": "/HowMany/index.html"
+  }
+];
